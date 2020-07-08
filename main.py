d5ca1b73523cfdce605fda1a459d42fedd9b2df6
Debzou <dapetit@grandlyon.com>
2020-07-08 13:45:00 +0200

    a very simple template

---
+++ main.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# import module.package
+from utils.function import *
+
+# main
+if __name__ == "__main__":
+    # checkimport()
+    checkimport()
---
+++ utils/function.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+def checkimport():
+    print('module : utils')
+    print('package : function')
